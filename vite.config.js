4b2fc8d2af7dfe9f909f986c83406599cf2de219
Escola <escolapjlr@outlook.com>
2025-04-16 17:16:50 -0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Substitua 'matricula-online' pelo nome exato do seu repositório no GitHub!
-export default defineConfig({
-  base: '/matricula-online/', 
-  plugins: [react()],
-})
